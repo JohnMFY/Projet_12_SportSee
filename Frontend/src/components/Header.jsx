ab7e9a85f1296f3e57b567fd435e87f83590f1d5
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-05-16 10:03:10 +0200

    Header and Nav WIP

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./Header.scss"
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <img src="Frontend\src\assets\LargeLogoSportSee.svg" alt="logo" />
+        <h2>Accueil</h2>
+        <h2>Profil</h2>
+        <h2>Réglages</h2>
+        <h2>Communauté</h2>
+    </div>
   )
 }
 
--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./Nav.scss"
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='Nav'>
+      <div className='icon'>
+        <i className="fa-solid fa-person-praying fa-2x"></i>
+        <i className="fa-solid fa-person-swimming fa-flip-horizontal fa-2x"></i>
+        <i className="fa-solid fa-person-biking fa-2x"></i>
+        <i className="fa-solid fa-dumbbell fa-2x"></i>
+      </div>
+      <p>Copyright, SportSee 2025</p>
+    </div>
   )
 }
 
