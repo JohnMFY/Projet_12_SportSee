9671d14bee7a7ae4aa246a1c6d902ae061989b0e
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-05-27 13:10:57 +0200

    Data fetch done

--- Frontend/src/components/Dashborad.jsx
+++ Frontend/src/components/Dashborad.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hello from './Hello'
 import DailyActivity from './DailyActivity'
 import SessionsDatas from './SessionsDatas'
 import Nutrition from './Nutrition'
 
-let test ='userName'
-
 function Dashborad() {
-    async function userData() {
-        try{
-            const data = await fetch(`http://localhost:3000/user/12`)
-            .then((data)=> data.json())
-            console.log(data)
-            return data
-        } catch(err){
-            console.log(err);
-        }
-    }userData()
+   const [data, setData] = useState(null)
+   
+    useEffect(()=>{
+        fetch(`http://localhost:3000/user/12`)
+        .then((res) => {
+            return res.json();
+        })
+        .then(data => {
+            setData(data)
+            console.log(data);
+        })
+    },[]);
     
   return (
     <div className='dashboard'>
+        {data &&
         <Hello
-        userName= {test}//.userInfos.firstName
-        />
+        userName= {data.data.userInfos.firstName}//.userInfos.firstName
+        />}
         <DailyActivity/>
         <SessionsDatas/>
         <Nutrition/>
